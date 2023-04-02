592fe8aeee8e5ac3a59e46f059cf60b2db0312fd
lohith warlock <lohxt.space@gmail.com>
2023-04-02 11:40:35 +0530

    feat: updated anim seq

--- containers/ui/memories.tsx
+++ containers/ui/memories.tsx
@@ -1,8 +1,9 @@
 import { motion } from "framer-motion";
 import { cn } from "@/utils/tailwind";
 
-const scale = 10;
+const scale = 15;
 
+// greater the number, greater the distortion
 const memories = [
   1, 4, 3, 5, 3, 4, 3, 4, 3, 1,
   // -
@@ -10,11 +11,11 @@ const memories = [
   // -
   1, 4, 5, 4, 3, 3, 3, 3, 3, 1,
   // -
-  1, 3, 4, 2, 2, 5, 5, 1, 5, 1,
+  1, 3, 4, 2, 8, 5, 9, 1, 5, 1,
   // -
-  1, 3, 3, 2, 2, 5, 1, 2, 2, 2,
+  1, 3, 3, 2, 8, 8, 1, 2, 2, 2,
   // -
-  1, 3, 3, 2, 2, 6, 2, 0, 0, 0,
+  1, 3, 3, 2, 7, 9, 2, 0, 0, 0,
   // -
   1, 3, 3, 2, 2, 5, 2, 0, 0, 0,
   // -
@@ -22,21 +23,38 @@ const memories = [
 ]
   .map((_) => _ * scale)
   .map((val, idx) => ({
-    initial: { opacity: 1, y: 0, x: 0, background: `#f500` },
-    visible: {
-      opacity: 1,
-      y: Math.random() * val - val / 2,
-      x: Math.random() * val - val / 2,
-      background: val == 0 ? `#f50` : `#f500`,
-      transition: {
-        delay: 0.01 * idx,
-        duration: 1,
-        ease: [0.2, 0.65, 0.3, 0.9],
+    outer: {
+      initial: { opacity: 0, y: 20 },
+      visible: {
+        opacity: 1,
+        y: 0,
+        transition: {
+          delay: 0.03 * idx,
+          duration: 0.5,
+          ease: [0.2, 0.65, 0.3, 0.9],
+        },
       },
+      exit: {},
+      hover: {},
+      value: val,
     },
-    exit: {},
-    hover: { y: 0, x: 0 },
-    value: val,
+    inner: {
+      initial: { opacity: 1, y: 0, x: 0, background: `#f500` },
+      visible: {
+        opacity: 1,
+        y: Math.random() * val - val / 2,
+        x: Math.random() * val - val / 2,
+        background: val == 0 ? `#f50` : `#f500`,
+        transition: {
+          delay: 0.03 * idx + 2.5,
+          duration: 1.5,
+          ease: [0.2, 0.65, 0.3, 0.9],
+        },
+      },
+      exit: {},
+      hover: {},
+      value: val,
+    },
   }));
 
 const baseVariant = {
@@ -58,12 +76,20 @@ const Memories = () => {
           variants={baseVariant}
         >
           <motion.div
-            className={cn(
-              "absolute h-[70px] w-[70px] rounded-full",
-              "border border-[#f50]",
-            )}
-            variants={_}
-          ></motion.div>
+            className="relative m-[1px] h-[70px] w-[70px]"
+            initial="initial"
+            animate="visible"
+            exit="exit"
+            variants={_?.outer}
+          >
+            <motion.div
+              className={cn(
+                "absolute h-[70px] w-[70px] rounded-full",
+                "border border-[#f50]",
+              )}
+              variants={_?.inner}
+            ></motion.div>
+          </motion.div>
         </motion.div>
       ))}
 
